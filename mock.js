cbeec6474fc24e1c18c7493dfb6cae69bc876268
a.konopleva <a.konopleva@crazypanda.ru>
2022-05-19 22:05:37 +0300

    upd ws

--- mock.js
+++ mock.js
@@ -6,95 +6,100 @@
     start:
     node ./nymphea-mock/mock.js
 */
-const express = require('express')
-const cors = require('cors')
-const http = require('http')
-const WebSocket = require('ws')
-const { getFormattedWSLog, writeToFile } = require('./utils.js')
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const WebSocket = require('ws');
+const { getFormattedWSLog, writeToFile } = require('./utils.js');
+const dumpTestLog = require('./dump-log.json');
+//import dumpTestLog from `./dump-log.json` assert { type: `json` };
 
-const app = express()
+const app = express();
 const port = 8000;
-const jsonParser = express.json()
+const jsonParser = express.json();
 
-app.use(cors())
+app.use(cors());
 
 app.post('/mock', jsonParser, function (request, response) {
-    console.log('\nHTTP:', request.body)
-    if (!request.body) return response.sendStatus(400)
-    if(request.body.needError){
-        return response.sendStatus(400)
-    }
-    if(request.body.delay){
-        setTimeout((() => {
-            response.json(request.body.mockRequest)
-          }), request.body.delay)
-        return
-    }
-    response.json(request.body.mockRequest) // отправляем пришедший ответ обратно
-})
+  console.log('\nHTTP:', request.body);
+  if (!request.body) return response.sendStatus(400);
+  if (request.body.needError) {
+    return response.sendStatus(400);
+  }
+  if (request.body.delay) {
+    setTimeout(() => {
+      response.json(request.body.mockRequest);
+    }, request.body.delay);
+    return;
+  }
+  response.json(request.body.mockRequest); // отправляем пришедший ответ обратно
+});
 
 app.get('/', (request, response) => {
-    response.send('GET NOT SUPPORTED! use only post')
-})
+  response.send('GET NOT SUPPORTED! use only post');
+});
 
-const server = http.createServer(app)
-const wss = new WebSocket.Server({ server })
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (socket) => {
-  let streams
+  console.log('WS connection');
+  let streams;
 
-  const isStreamCancelled = stream => !streams.has(stream)
-  const abortStreams = () => streams = new WeakSet()
+  const isStreamCancelled = (stream) => !streams.has(stream);
+  const abortStreams = () => (streams = new WeakSet());
   const addStream = () => {
-    const pointer = {}
-    streams.add(pointer)
-    return pointer
-  }
+    const pointer = {};
+    streams.add(pointer);
+    return pointer;
+  };
 
-  abortStreams()
+  abortStreams();
 
   socket.on('message', (message) => {
+    console.log('WS send message');
     try {
-      const { realRequest, mockRequest } = JSON.parse(message)
+      const { realRequest, mockRequest } = JSON.parse(message);
 
-      console.log('\nWS:', realRequest)
-      writeToFile('logs', 'websocket_last_request.txt', getFormattedWSLog(realRequest, mockRequest))
+      console.log('\nWS:', realRequest);
+      writeToFile('logs', 'websocket_last_request.txt', getFormattedWSLog(realRequest, mockRequest));
 
-      if (!mockRequest) return
+      if (!mockRequest) return;
 
-      const { mockResponse, splitIntoChunks, stopAll, delay } = mockRequest
-      if (stopAll) abortStreams()
-      if (!mockResponse) return
-
-      const responseData = (Array.isArray(mockResponse) && splitIntoChunks)
-        ? mockResponse
-        : [mockResponse]
-
+      const { mockResponse, useFile, chunkLength, limit, stopAll, delay } = mockRequest;
+      if (stopAll) abortStreams();
+      if (!mockResponse && !useFile) return;
+      let responseData;
+      if (useFile) {
+        responseData = dumpTestLog;
+      } else {
+        responseData = Array.isArray(mockResponse) ? mockResponse : [mockResponse];
+      }
       function* chunkGenerator() {
-        let i = 0
-        while (i < responseData.length) {
-          yield new Promise(resolve => {
-            setTimeout(resolve, delay || 0)
-          }).then(() => responseData[i++])
+        let i = 0;
+        while (i < limit) {
+          yield new Promise((resolve) => {
+            setTimeout(resolve, delay || 0);
+          }).then(() => responseData.slice(i, (i += chunkLength)));
         }
       }
 
-      const stream = addStream()
-      ;(async function () {
+      const stream = addStream();
+      (async function () {
         for await (const chunk of chunkGenerator()) {
-          if (isStreamCancelled(stream)) return
-          socket.send(JSON.stringify(chunk))
+          if (isStreamCancelled(stream)) return;
+          socket.send(JSON.stringify(chunk));
         }
-      })()
+      })();
     } catch (err) {
-      console.log('WebSocket error:', err.message)
+      console.log('WebSocket error:', err.message);
     }
-  })
-})
+  });
+});
 
 server.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err)
-    }
-    console.log(`HTTP and WebSocket servers are listening the port ${port}`)
-})
+  if (err) {
+    return console.log('Something bad happened', err);
+  }
+  console.log(`HTTP and WebSocket servers are listening the port ${port}`);
+});
