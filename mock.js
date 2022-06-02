a82359f03581cb81f6a93e2f5cbba72e9d09de90
a.konopleva <a.konopleva@crazypanda.ru>
2022-06-02 23:02:55 +0300

    add default dump file

--- mock.js
+++ mock.js
@@ -2,16 +2,17 @@
     dependency:
     "cors": "^2.8.5",
     "express": "^4.17.1",
+    npm i ws
 
     start:
     node ./nymphea-mock/mock.js
 */
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const WebSocket = require('ws');
-const { getFormattedWSLog, writeToFile } = require('./utils.js');
-const dumpTestLog = require('./dump-log.json');
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const WebSocket = require("ws");
+const { getFormattedWSLog, writeToFile } = require("./utils.js");
+const dumpTestLog = require("./dump-log.json");
 //import dumpTestLog from `./dump-log.json` assert { type: `json` };
 
 const app = express();
@@ -20,8 +21,8 @@ const jsonParser = express.json();
 
 app.use(cors());
 
-app.post('/mock', jsonParser, function (request, response) {
-  console.log('\nHTTP:', request.body);
+app.post("/mock", jsonParser, function (request, response) {
+  console.log("\nHTTP:", request.body);
   if (!request.body) return response.sendStatus(400);
   if (request.body.needError) {
     return response.sendStatus(400);
@@ -35,15 +36,15 @@ app.post('/mock', jsonParser, function (request, response) {
   response.json(request.body.mockRequest); // отправляем пришедший ответ обратно
 });
 
-app.get('/', (request, response) => {
-  response.send('GET NOT SUPPORTED! use only post');
+app.get("/", (request, response) => {
+  response.send("GET NOT SUPPORTED! use only post");
 });
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (socket) => {
-  console.log('WS connection');
+wss.on("connection", (socket) => {
+  console.log("WS connection");
   let streams;
 
   const isStreamCancelled = (stream) => !streams.has(stream);
@@ -56,24 +57,31 @@ wss.on('connection', (socket) => {
 
   abortStreams();
 
-  socket.on('message', (message) => {
-    console.log('WS send message');
+  socket.on("message", (message) => {
+    console.log("WS send message");
     try {
       const { realRequest, mockRequest } = JSON.parse(message);
 
-      console.log('\nWS:', realRequest);
-      writeToFile('logs', 'websocket_last_request.txt', getFormattedWSLog(realRequest, mockRequest));
+      console.log("\nWS:", realRequest);
+      writeToFile(
+        "logs",
+        "websocket_last_request.txt",
+        getFormattedWSLog(realRequest, mockRequest)
+      );
 
       if (!mockRequest) return;
 
-      const { mockResponse, useFile, chunkLength, limit, stopAll, delay } = mockRequest;
+      const { mockResponse, useFile, chunkLength, limit, stopAll, delay } =
+        mockRequest;
       if (stopAll) abortStreams();
       if (!mockResponse && !useFile) return;
       let responseData;
       if (useFile) {
         responseData = dumpTestLog;
       } else {
-        responseData = Array.isArray(mockResponse) ? mockResponse : [mockResponse];
+        responseData = Array.isArray(mockResponse)
+          ? mockResponse
+          : [mockResponse];
       }
       function* chunkGenerator() {
         let i = 0;
@@ -92,14 +100,14 @@ wss.on('connection', (socket) => {
         }
       })();
     } catch (err) {
-      console.log('WebSocket error:', err.message);
+      console.log("WebSocket error:", err.message);
     }
   });
 });
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('Something bad happened', err);
+    return console.log("Something bad happened", err);
   }
   console.log(`HTTP and WebSocket servers are listening the port ${port}`);
 });
